165442b61172348ec7f8ec50b6a522d0a70d0693
Ricardo Ianck <ricardoianck.vendas@gmail.com>
2023-11-28 14:10:39 -0300

    alterando frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home(){
-    return <h1>Bruna eu te amo ........se vc me ama tambem da um sorriso para a camera  </h1>
+    return <h1>Luciano , tenho muito orgulho da nossa parceria .....vamos ter sucesso juntos ...... dallleeeee LOGGIC  </h1>
 }
 
 export default Home;
